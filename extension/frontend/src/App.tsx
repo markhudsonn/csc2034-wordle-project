a5b4441dd54c66653fef27f328373da58047e4d7
markhudsonn <markhudsonn4@gmail.com>
2024-03-03 17:11:55 +0000

    frontend style changes

--- extension/frontend/src/App.tsx
+++ extension/frontend/src/App.tsx
@@ -3,13 +3,14 @@ import axios from 'axios'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
 import { Input } from '@/components/ui/input'
+import { IoIosRefresh } from "react-icons/io";
 import './globals.css'
 import './App.css'
 
 const API_URL = "http://127.0.0.1:5000/api";
 
 function App() {
-  const [state, setState] = useState("");
+  const [state, setState] = useState("Connecting...");
   const [guess, setGuess] = useState("");
   const [guesses, setGuesses] = useState([]);
   const [hint, setHint] = useState("");
@@ -29,11 +30,11 @@ function App() {
     setGuesses(response.data.guesses);
   }
 
-  const handleGuessChange = (e) => {
+  const handleGuessChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setGuess(e.target.value);
   }
 
-  const handleGuessSubmit = async (e) => {
+  const handleGuessSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     const response = await axios.post(`${API_URL}/make_guess`, {word: guess});
     if (response.data.message === "Guess made") {
@@ -58,18 +59,30 @@ function App() {
     }
   }
 
+  const returnGuesses = () => {
+    if (guesses.length === 0) {
+      return <li>No guesses yet</li>;
+    }
+    return guesses.map((guess, index) => (
+      <li key={index}>{guess}</li>
+    ));
+  }
+
   return (
     <div className="App">
-      <h1>Wordle</h1>
+      <h1 style={{fontSize: '2em', fontWeight: 'bold'}}>CSC2034 Wordle Game</h1>
       <b>Game status: {state}</b>
-      <Separator />
-      <Input type="text" value={guess} onChange={handleGuessChange} placeholder="Your guess here..." />
-      <Button onClick={handleGuessSubmit}>Submit Guess</Button>
-      <Button onClick={getHint}>Get Hint</Button>
-      <Button onClick={handleNewGame}>New Game</Button>
+      <Separator className="my-10" />
+      {returnGuesses()}
+      <div style={{ margin: '0 auto', width: '50%' }}>
+        <Input className="w-full" type="text" value={guess} onChange={handleGuessChange} placeholder="Your guess here..." />
+        <Button className="m-2" onClick={handleGuessSubmit}>Submit Guess</Button>
+        <Button className="m-2" onClick={getHint}>Get Hint</Button>
+      </div>
+      <Button className="m-2" variant="secondary" onClick={handleNewGame}><IoIosRefresh /></Button>
       <br/>
       {hint && <div>Hint: {hint}</div>}
-      {guesses.map((guess, i) => <div key={i}>{guess}</div>)}
+      {state === "WON" && <div>Congratulations! You won!</div>}
     </div>
   )
 }
