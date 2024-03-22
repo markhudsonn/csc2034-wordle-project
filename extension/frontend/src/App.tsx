f65a063e57f1325b1621e7f6ac4f6fd416a739cf
markhudsonn <markhudsonn4@gmail.com>
2024-03-22 23:24:58 +0000

    hosting config frontend

--- extension/frontend/src/App.tsx
+++ extension/frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
