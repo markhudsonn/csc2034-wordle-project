ec739b5c61fe5332cbd6cc47415f452a152badf6
markhudsonn <markhudsonn4@gmail.com>
2024-03-24 22:24:18 +0000

    Update Footer component with GitHub link

--- extension/frontend/src/components/Footer.tsx
+++ extension/frontend/src/components/Footer.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { IoLogoGithub } from "react-icons/io";
+
 
 const Footer = () => (
-  <footer style={{ position: 'fixed', bottom: '0', left: '0', right: '0', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '10px', backgroundColor: '#f0f0f0', color: 'grey' }}>
+  <footer style={{ position: 'fixed', bottom: '0', left: '0', right: '0', display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '8px', backgroundColor: '#f0f0f0', color: 'grey' }}>
     <div style={{ marginLeft: '10px' }}>
       <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank" rel="noopener noreferrer" style={{ color: 'grey', textDecoration: 'none' }}>Inspired by Wordle (NYT)</a>
     </div>
-    <span style={{ margin: '0 10px' }}>|</span>
-    <div style={{ marginRight: '10px' }}>
+    <div>
       <p>Mark Hudson © 2024</p>
+    </div>
+    <div style={{ marginRight: '10px' }}>
+      <a href="https://github.com/markhudsonn/csc2034-wordle-project" target="_blank" rel="noopener noreferrer" style={{ color: 'grey', textDecoration: 'none' }}>
+        <IoLogoGithub size={20} />
+      </a>
     </div>
   </footer>
 );
